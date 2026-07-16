112ddbf2e09d9cb4736de70dd01eb9654cf01d70
mgalliou <matthias.galliou@gmail.com>
2026-07-16 10:26:18 +0200

    refactor: extract make_item and is_triggered helpers

--- lua/blink-cmp-tmux/init.lua
+++ lua/blink-cmp-tmux/init.lua
@@ -37,6 +37,30 @@ function tmux:get_trigger_characters()
 	return self.opts.trigger_chars
 end
 
+---@param word string
+---@param context blink.cmp.Context
+---@return lsp.CompletionItem
+function tmux:make_item(word, context)
+	---@type lsp.CompletionItem
+	local item = {
+		label = word,
+		kind = require("blink.cmp.types").CompletionItemKind.Text,
+		insertText = word,
+	}
+
+	if self.opts.triggered_only then
+		item.textEdit = {
+			newText = word,
+			range = {
+				start = { line = context.cursor[1] - 1, character = context.bounds.start_col - 2 },
+				["end"] = { line = context.cursor[1] - 1, character = context.cursor[2] },
+			},
+		}
+	end
+
+	return item
+end
+
 ---@param pane_id string
 ---@return string
 function tmux:get_pane_content(pane_id)
@@ -47,25 +71,6 @@ function tmux:get_pane_content(pane_id)
 	end
 
 	return vim.system(cmd, { text = true }):wait().stdout or ""
-end
-
----@return string[]
-function tmux:get_words()
-	local words = {}
-
-	vim.iter(self:get_pane_ids()):each(function(id)
-		-- match not only full words, but urls, paths, etc.
-		vim.iter(string.gmatch(self:get_pane_content(id), "[%w%d_:/.%-~]+")):each(function(word)
-			words[word] = true
-
-			-- but also isolate the words from the result
-			for sub_word in string.gmatch(word, "[%w%d]+") do
-				words[sub_word] = true
-			end
-		end)
-	end)
-
-	return vim.tbl_keys(words)
 end
 
 ---@return string[]
@@ -94,29 +99,46 @@ function tmux:get_pane_ids()
 	return ids
 end
 
+---@return string[]
+function tmux:get_words()
+	local words = {}
+
+	vim.iter(self:get_pane_ids()):each(function(id)
+		-- match not only full words, but urls, paths, etc.
+		vim.iter(string.gmatch(self:get_pane_content(id), "[%w%d_:/.%-~]+")):each(function(word)
+			words[word] = true
+
+			-- but also isolate the words from the result
+			for sub_word in string.gmatch(word, "[%w%d]+") do
+				words[sub_word] = true
+			end
+		end)
+	end)
+
+	return vim.tbl_keys(words)
+end
+
+---@param context blink.cmp.Context
+---@return boolean
+function tmux:is_triggered(context)
+	if not self.opts.triggered_only then
+		return true
+	end
+	local char = context.line:sub(context.bounds.start_col - 1, context.bounds.start_col - 1)
+
+	return vim.list_contains(self:get_trigger_characters(), char)
+end
+
 ---@param context blink.cmp.Context
 ---@return lsp.CompletionItem[]
 function tmux:get_completion_items(context)
+	if not self:is_triggered(context) then
+		return {}
+	end
+
 	return vim.iter(self:get_words())
 		:map(function(word)
-			---@type lsp.CompletionItem
-			local item = {
-				label = word,
-				kind = require("blink.cmp.types").CompletionItemKind.Text,
-				insertText = word,
-			}
-			if self.opts.triggered_only then
-				item = vim.tbl_deep_extend("force", item, {
-					textEdit = {
-						newText = word,
-						range = {
-							start = { line = context.cursor[1] - 1, character = context.bounds.start_col - 2 },
-							["end"] = { line = context.cursor[1] - 1, character = context.cursor[2] },
-						},
-					},
-				})
-			end
-			return item
+			return self:make_item(word, context)
 		end)
 		:totable()
 end
@@ -125,13 +147,8 @@ end
 ---@param callback fun(items: blink.cmp.CompletionItem[])
 function tmux:get_completions(context, callback)
 	vim.schedule(function()
-		local triggered = not self.opts.triggered_only
-			or vim.list_contains(
-				self:get_trigger_characters(),
-				context.line:sub(context.bounds.start_col - 1, context.bounds.start_col - 1)
-			)
 		callback({
-			items = triggered and self:get_completion_items(context) or {},
+			items = self:get_completion_items(context),
 			is_incomplete_backward = true,
 			is_incomplete_forward = true,
 		})
